8c74095230673990d3a5c865e936a8914ea9f139
Luke Plant <L.Plant.98@cantab.net>
2015-08-20 12:44:26 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = requirements
 
 setup(
     name='django_custom_user_migration',
-    version='0.1.0',
+    version='0.2.0',
     description="django_custom_user_migration will help you create a migration to using a custom User model with Django",
     long_description=readme + '\n\n' + history,
     author="Luke Plant",
